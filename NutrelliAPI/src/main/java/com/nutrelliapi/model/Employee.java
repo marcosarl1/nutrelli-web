0ddde9609279202d0b576ce24f9f7174eb9b38c9
Marcos Júnior <marcosajr11@gmail.com>
2025-01-02 22:07:52 -0300

    feat: Employee class

---
+++ NutrelliAPI/src/main/java/com/nutrelliapi/model/Employee.java
@@ -0,0 +1,29 @@
+package com.nutrelliapi.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String name;
+
+    private String email;
+
+    private String password;
+
+    public Employee() {}
+
+    public Employee(Integer id, String name, String email, String password) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.password = password;
+    }
+}
