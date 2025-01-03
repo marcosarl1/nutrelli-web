8f99897b0ca85b8694be9607c373b64b3dde1879
Marcos Júnior <marcosajr11@gmail.com>
2025-01-02 22:09:05 -0300

    first commit

---
+++ src/test/java/com/nutrelliapi/NutrelliApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nutrelliapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NutrelliApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
