e0882df08887524fd14c7e9133e6b2bf68071be5
Marcos Júnior <marcosajr11@gmail.com>
2025-01-04 15:13:06 -0300

    feat: Jwt Token implementation

---
+++ NutrelliAPI/src/main/java/com/nutrelliapi/security/JwtTokenFilter.java
@@ -0,0 +1,67 @@
+package com.nutrelliapi.security;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.interfaces.DecodedJWT;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.util.Collections;
+import java.util.Date;
+
+public class JwtTokenFilter extends OncePerRequestFilter {
+
+    private static final String SECRET_KEY = "chavemuitosecretahehehhe";
+    private static final long EXPIRATION_TIME = 86400000; // 1 day in ms
+
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String token = extractToken(request);
+        if (token != null && validateToken(token)) {
+            Authentication auth = getAuth(token);
+            SecurityContextHolder.getContext().setAuthentication(auth);
+        }
+        filterChain.doFilter(request, response);
+    }
+
+    public static String generateToken(String email) {
+        return JWT.create()
+                .withSubject(email)
+                .withExpiresAt(new Date(System.currentTimeMillis() + EXPIRATION_TIME))
+                .sign(Algorithm.HMAC256(SECRET_KEY));
+    }
+
+    private boolean validateToken(String token) {
+        try {
+            Algorithm algorithm = Algorithm.HMAC256(SECRET_KEY);
+            JWT.require(algorithm)
+                    .build()
+                    .verify(token);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    private String extractToken(HttpServletRequest request) {
+        String bearerToken = request.getHeader("Authorization");
+        if (bearerToken != null && bearerToken.startsWith("Bearer ")) {
+            return bearerToken.substring(7);
+        }
+        return null;
+    }
+
+    private Authentication getAuth(String token) {
+        DecodedJWT decodedJWT = JWT.decode(token);
+        String email = decodedJWT.getSubject();
+        return new UsernamePasswordAuthenticationToken(email, null, Collections.emptyList());
+    }
+}
