f280bb74c8fbecd4693aa6b3faabf028ee18d011
Marcos Júnior <marcosajr11@gmail.com>
2025-01-04 11:58:04 -0300

    feat: Added 'name' attribute

--- src/main/java/com/nutrelliapi/dto/EmployeeDTO.java
+++ src/main/java/com/nutrelliapi/dto/EmployeeDTO.java
@@ -1,22 +1,19 @@
 package com.nutrelliapi.dto;
 
-public class LoginDTO {
+public class EmployeeDTO {
+    private String name;
     private String email;
     private String password;
+
+    public String getName() {
+        return name;
+    }
 
     public String getEmail() {
         return email;
     }
 
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
     public String getPassword() {
         return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
     }
 }
