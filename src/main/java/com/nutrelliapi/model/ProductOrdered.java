bbe0278348244f9ea24d7285905f7b329f45cdb3
Marcos Júnior <marcosajr11@gmail.com>
2025-01-05 19:59:32 -0300

    feat: ProductOrdered class

---
+++ src/main/java/com/nutrelliapi/model/ProductOrdered.java
@@ -0,0 +1,64 @@
+package com.nutrelliapi.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "produto_pedido")
+public class ProductOrdered {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @ManyToOne
+    @JoinColumn(name = "id_pedido")
+    private Order order;
+
+    @ManyToOne
+    @JoinColumn(name = "id_produto")
+    private Product product;
+
+    @Column(name = "quantidade")
+    private int quantity;
+
+    public ProductOrdered(Integer id, Order order, Product product, int quantity) {
+        this.id = id;
+        this.order = order;
+        this.product = product;
+        this.quantity = quantity;
+    }
+
+    public ProductOrdered() {}
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Order getOrder() {
+        return order;
+    }
+
+    public void setOrder(Order order) {
+        this.order = order;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+}
