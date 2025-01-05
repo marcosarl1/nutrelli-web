d022968766f99a66c34b7bee39c8859435401605
Marcos Júnior <marcosajr11@gmail.com>
2025-01-05 11:59:05 -0300

    remove: Removed not necessary html tags

--- nutrelli_front-end/src/app/dashboard/layout.js
+++ nutrelli_front-end/src/app/dashboard/layout.js
@@ -6,11 +6,9 @@ import {useState} from "react";
 export default function DashboardLayout({ children }) {
     const [isSidebarOpen, setIsSidebarOpen] = useState(true)
     return (
-        <html lang="en">
-            <body>
+            <div className="d-flex">
                 <Sidebar isOpen={isSidebarOpen} setIsOpen={setIsSidebarOpen}/>
                 <main className="d-flex flex-grow-1 p-4" style={{marginLeft: isSidebarOpen ? '280px' : '80px', transition: 'margin-left 0.3s ease'}}>{children}</main>
-            </body>
-        </html>
+            </div>
     );
 }
