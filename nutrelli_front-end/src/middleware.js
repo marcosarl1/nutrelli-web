23a662aac0956e0995ec5cc894baddc30355cfcb
Marcos Júnior <marcosajr11@gmail.com>
2025-01-05 13:35:26 -0300

    feat: Middleware to protect routes

---
+++ nutrelli_front-end/src/middleware.js
@@ -0,0 +1,33 @@
+import { NextResponse } from "next/server";
+import { authenticate } from "@/services/loginService";
+
+export async function middleware(req) {
+    const jwtToken = req.cookies.get('jwt_token');
+    const {pathname} = req.nextUrl;
+
+    const protectedRoutes = ['/dashboard'];
+    const publicRoutes = ['/', '/login'];
+
+    const isProtectedRoute = protectedRoutes.some(route => pathname.startsWith(route));
+    const isPublicRoute = publicRoutes.includes(pathname);
+
+
+    if (jwtToken) {
+        const isValid = await authenticate(jwtToken.value);
+        if (!isValid && isProtectedRoute) {
+            return NextResponse.redirect(new URL('/login', req.url));
+        }
+
+        if (isValid && isPublicRoute) {
+            return NextResponse.redirect(new URL('/dashboard', req.url));
+        }
+    } else if (isProtectedRoute) {
+        return NextResponse.redirect(new URL('/login', req.url));
+    }
+
+    return NextResponse.next();
+}
+
+export const config = {
+    matcher: ['/', '/login','/dashboard/:path*'],
+};
