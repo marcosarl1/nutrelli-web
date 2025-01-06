5a3b44258ec84d867a10984af543eecd4b6c7eb6
Marcos Júnior <marcosajr11@gmail.com>
2025-01-05 22:29:44 -0300

    feat: OrderController class

---
+++ NutrelliAPI/src/main/java/com/nutrelliapi/controller/OrderController.java
@@ -0,0 +1,43 @@
+package com.nutrelliapi.controller;
+
+import com.nutrelliapi.model.Order;
+import com.nutrelliapi.service.impl.OrderService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/orders")
+public class OrderController {
+
+    private final OrderService orderService;
+
+    public OrderController(OrderService orderService) {
+        this.orderService = orderService;
+    }
+
+    @GetMapping
+    public List<Order> findAllOrders() {
+        return orderService.findAllOrders();
+    }
+
+    @GetMapping("/{id}")
+    public Order findOrderById(@PathVariable Integer id) {
+        return orderService.findOrderById(id);
+    }
+
+    @PostMapping("/add")
+    public Order saveOrder(@RequestBody Order order) {
+        return orderService.saveOrder(order);
+    }
+
+    @PutMapping("/update/{id}")
+    public Order updateOrder(@PathVariable Integer id, @RequestBody Order order) {
+        return orderService.updateOrder(id, order);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public void deleteOrder(@PathVariable Integer id) {
+        orderService.deleteOrder(id);
+    }
+}
