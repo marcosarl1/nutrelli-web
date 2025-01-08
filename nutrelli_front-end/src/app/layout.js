b261c4e9f90421da64754b5e31a3409f9a9532eb
Marcos Júnior <marcosajr11@gmail.com>
2025-01-08 00:09:52 -0300

    feat: Added viewport

--- nutrelli_front-end/src/app/layout.js
+++ nutrelli_front-end/src/app/layout.js
@@ -3,8 +3,12 @@ import './globals.css';
 export default function RootLayout({ children }) {
   return (
       <html lang="en">
-        <body>{children}
-        </body>
+          <head>
+              <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+              <title>Nutrelli</title>
+          </head>
+          <body>{children}
+          </body>
       </html>
   );
 }
