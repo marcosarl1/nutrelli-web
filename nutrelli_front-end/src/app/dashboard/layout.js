b58823de47ecaf07c8f8c861665d9390f37f2f3e
Marcos Júnior <marcosajr11@gmail.com>
2025-01-08 00:08:28 -0300

    feat: added paddings and margins to layout

--- nutrelli_front-end/src/app/dashboard/layout.js
+++ nutrelli_front-end/src/app/dashboard/layout.js
@@ -1,7 +1,6 @@
 'use client';
 
-import "./dashboard.css"
-import {SidebarProvider, SidebarTrigger} from "@/components/ui/sidebar";
+import {SidebarProvider} from "@/components/ui/sidebar";
 import {AppSidebar} from "@/app/dashboard/components/app-sidebar";
 import MobileHeader from "@/app/dashboard/components/mobile-header";
 
@@ -10,7 +9,7 @@ export default function DashboardLayout({ children }) {
         <SidebarProvider>
             <AppSidebar />
             <MobileHeader/>
-            <main className="p-4">
+            <main className="flex-1 p-4 lg:p-6 m-14 lg:mt-0">
                 {children}
             </main>
         </SidebarProvider>
