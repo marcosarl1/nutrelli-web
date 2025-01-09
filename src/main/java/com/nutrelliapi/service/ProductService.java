7430320f7870d1a31dd9f060c0b5e8a4f58cb08c
Marcos Júnior <marcosajr11@gmail.com>
2025-01-09 01:25:26 -0300

    feat: Methods findAllProductsPage and findAllProductsContaining

--- src/main/java/com/nutrelliapi/service/ProductService.java
+++ src/main/java/com/nutrelliapi/service/ProductService.java
@@ -1,13 +1,15 @@
 package com.nutrelliapi.service;
 
 import com.nutrelliapi.model.Product;
+import org.springframework.data.domain.Page;
 
 import java.util.List;
 
 public interface ProductService {
     List<Product> findAllProducts();
+    Page<Product> findAllProductsPage(int page, int size);
     Product findProductById(Integer id);
-    List<Product> findProductLike(String query);
+    List<Product> findAllProductsContaining(String query);
     Product saveProduct(Product product);
     Product updateProduct(Integer id, Product product);
     void deleteProduct(Integer id);
