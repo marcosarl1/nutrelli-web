2891bcef75aadc2b11f64a6a39aa1e1bd2238bc1
Marcos Júnior <marcosajr11@gmail.com>
2025-01-09 01:40:22 -0300

    refactor: Correct name

--- NutrelliAPI/src/main/java/com/nutrelliapi/exception/ProductCategoryNotFoundException.java
+++ NutrelliAPI/src/main/java/com/nutrelliapi/exception/ProductCategoryNotFoundException.java
@@ -4,8 +4,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(HttpStatus.NOT_FOUND)
-public class ProductCategoryException extends RuntimeException {
-    public ProductCategoryException(String message) {
+public class ProductCategoryNotFoundException extends RuntimeException {
+    public ProductCategoryNotFoundException(String message) {
         super(message);
     }
 }
