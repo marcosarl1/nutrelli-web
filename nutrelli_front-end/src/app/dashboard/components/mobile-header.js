970bdc3f0b8055f8bc16f1dc2a88cdea3a9a8a11
Marcos Júnior <marcosajr11@gmail.com>
2025-01-11 22:19:48 -0300

    feat: mobile-header only showing on small display

--- nutrelli_front-end/src/app/dashboard/components/mobile-header.js
+++ nutrelli_front-end/src/app/dashboard/components/mobile-header.js
@@ -5,7 +5,7 @@ import {Menu} from "lucide-react";
 export default function MobileHeader() {
     const {toggleSidebar} = useSidebar();
     return (
-        <div className="lg:hidden fixed top-0 left-0 right-0 z-50">
+        <div className="lg:hidden md:hidden fixed top-0 left-0 right-0 z-50">
             <header className="bg-gradient-to-br from-orange-100 to to-orange-100 px-4 py-3 flex items-center justify-start shadow-sm">
                 <Button
                     variant="ghost"
