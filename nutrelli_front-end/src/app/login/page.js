c8b2e7f88cdf4455628ef931ad4280deea85dadd
Marcos Júnior <marcosajr11@gmail.com>
2025-01-12 15:34:02 -0300

    feat: Improved login page

--- nutrelli_front-end/src/app/login/page.js
+++ nutrelli_front-end/src/app/login/page.js
@@ -3,7 +3,7 @@
 import Image from 'next/image';
 import {useRouter} from 'next/navigation';
 import {useState} from "react";
-import Logo from '../../../public/nutrelli-logo.png';
+import Logo from '../../../public/nutrelli.svg';
 import {AlertCircle, Eye, EyeOff, Loader2, Lock, Mail} from "lucide-react";
 import {login} from "@/services/loginService";
 import {Card, CardContent} from "@/components/ui/card";
@@ -73,99 +73,95 @@ export default function Login() {
     const isSubmitDisabled = loading || !formData.email || !formData.password;
 
     return (
-        <div className="min-h-screen flex items-center justify-center p-4 bg-gradient-to-br from-orange-100 to-gray-50">
-            <div className="w-full max-w-md">
-                <Card className="border-0 shadow-xl backdrop-blur-sm bg-white/90">
-                    <CardContent className="p-6 md:p-8">
-                        <div className="text-center mb-8">
-                            <div
-                                className="inline-flex items-center justify-center rounded-full bg-white shadow-lg mb-6">
-                                <Image
-                                    src={Logo} alt="Nutrelli Logo"
-                                    width={125}
-                                    height={125}
-                                    priority
-                                    className="h-auto w-auto"/>
-                            </div>
-                            <h1 className="text-2xl font-bold text-gray-800">Bem-vindo!</h1>
-                            <p className="text-gray-600">Entre com suas credenciais para acessar sua conta</p>
+        <div className="min-h-screen flex items-center justify-center p-4 bg-[url('/login-bg.png')] bg-cover bg-center">
+            <div className="w-full max-w-md ">
+                <div className="relative">
+                    <div className="absolute left-1/2 -top-14 -translate-x-1/2 -translate-y-1/2 ">
+                        <div
+                            className="h-28 w-56 bg-[#FFFCF5] rounded-tl-full rounded-tr-full flex items-center justify-center shadow-inner">
+                            <Image
+                                src={Logo} alt="Nutrelli Logo"
+                                priority/>
                         </div>
-                        {error && (
-                            <Alert variant="destructive" className="mb-6 flex items-center">
-                                <div className="flex items-center gap-2">
-                                    <AlertCircle className="h-4 w-4"/>
-                                    <AlertDescription className="">{error}</AlertDescription>
-                                </div>
-                            </Alert>
-                        )}
-                        <form noValidate onSubmit={handleSubmit} className="space-y-6">
-                            <div className="space-y-2">
-                                <Label htmlFor="email" className="text-gray-700 font-medium">E-mail</Label>
-                                <div className="relative">
-                                    <div className="absolute inset-y-0 left-0 flex items-center pl-3">
-                                        <Mail className="h-4 w-4 text-gray-400"/>
-                                    </div>
-                                    <Input
-                                        type="email"
-                                        id="email"
-                                        name="email"
-                                        placeholder="seu@email.com"
-                                        value={formData.email}
-                                        onChange={handleChange}
-                                        className={cn("pl-10 py-2 transition-colors",
-                                            touched.email && getFieldError('email') ? "border-red-500 focus:border-red-500" : "")}
-                                    aria-invalid={!!getFieldError('email')}/>
-                                </div>
-                                <div>
-                                    {touched.email && getFieldError('email') && (
-                                        <p className="mt-1 text-sm text-red-500">{getFieldError('email')}</p>
-                                    )}
-                                </div>
+                    </div>
+                    <Card className="border-0 shadow-2xl bg-[#FFFCF5] py-6">
+                        <CardContent className="p-8">
+                            <div className="text-center mb-8">
+                                <h1 className="text-4xl font-bold text-red-700">Bem-vindo!</h1>
                             </div>
-                            <div className="space-y-2">
-                                <Label htmlFor='password' className="text-gray-700 font-medium">Senha</Label>
-                                <div className="relative">
-                                    <div className="absolute inset-y-0 left-0 flex items-center pl-3">
-                                        <Lock className="w-4 h-4 text-gray-400"/>
+                            {error && (
+                                <Alert variant="destructive" className="mb-6 flex items-center">
+                                    <div className="flex items-center gap-2">
+                                        <AlertCircle className="h-4 w-4"/>
+                                        <AlertDescription className="">{error}</AlertDescription>
                                     </div>
-                                    <Input
-                                        type={showPassword ? 'text' : 'password'}
-                                        id="password"
-                                        name="password"
-                                        placeholder="Digite sua senha"
-                                        value={formData.password}
-                                        onChange={handleChange}
-                                        className={`pl-10 pr-10 py-2 transition-colors 
-                                        ${touched.password && getFieldError('password') ? 'border-red-500 focus:border-red-500' : ''}`}
-                                        aria-invalid={!!getFieldError('password')}/>
-                                    <button
-                                        type="button"
-                                        onClick={() => setShowPassword(!showPassword)}
-                                        className="absolute inset-y-0 right-0 flex items-center px-3 text-gray-400 hover:text-gray-600 transition-colors"
-                                        aria-label={showPassword ? 'Ocultar senha' : 'Mostrar senha'}>
-                                        {showPassword ? <EyeOff className="h-4 w-4"/> : <Eye className="h-4 w-4"/>}
-                                    </button>
+                                </Alert>
+                            )}
+                            <form noValidate onSubmit={handleSubmit} className="space-y-6">
+                                <div className="pb-4">
+                                    <div className="relative">
+                                        <Label htmlFor="email" className="absolute -top-3 left-3 bg-[#FFFCF5] px-1 text-sm text-gray-500">E-mail</Label>
+                                        <Input
+                                            type="email"
+                                            id="email"
+                                            name="email"
+                                            placeholder="seu@email.com"
+                                            value={formData.email}
+                                            onChange={handleChange}
+                                            className={cn("pt-3 pb-2 px-4 border border-gray-300 rounded-md w-full",
+                                                touched.email && getFieldError('email') ? "border-red-500 focus:border-red-500" : "")}
+                                            aria-invalid={!!getFieldError('email')}/>
+                                    </div>
+                                    <div>
+                                        {touched.email && getFieldError('email') && (
+                                            <p className="mt-1 text-sm text-red-500">{getFieldError('email')}</p>
+                                        )}
+                                    </div>
                                 </div>
-                                <div>
-                                    {touched.password && getFieldError('password') && (
-                                        <p className="mt-1 text-sm text-red-500">{getFieldError('password')}</p>
-                                    )}
+                                <div className="pb-4">
+                                    <div className="relative">
+                                        <Label htmlFor='password' className="absolute -top-3 left-3 bg-[#FFFCF5] px-1 text-sm text-gray-500">Senha</Label>
+                                        <Input
+                                            type={showPassword ? 'text' : 'password'}
+                                            id="password"
+                                            name="password"
+                                            placeholder="Digite sua senha"
+                                            value={formData.password}
+                                            onChange={handleChange}
+                                            className={cn("pt-3 pb-2 px-4 border border-gray-300 rounded-md w-full",
+                                                touched.password && getFieldError('password') ? 'border-red-500 focus:border-red-500' : '')}
+                                            aria-invalid={!!getFieldError('password')}/>
+                                        <button
+                                            type="button"
+                                            onClick={() => setShowPassword(!showPassword)}
+                                            className="absolute inset-y-0 right-0 flex items-center px-3 text-gray-400 hover:text-gray-600 transition-colors"
+                                            aria-label={showPassword ? 'Ocultar senha' : 'Mostrar senha'}>
+                                            {showPassword ? <EyeOff className="h-4 w-4"/> : <Eye className="h-4 w-4"/>}
+                                        </button>
+                                    </div>
+                                    <div>
+                                        {touched.password && getFieldError('password') && (
+                                            <p className="mt-1 text-sm text-red-500">{getFieldError('password')}</p>
+                                        )}
+                                    </div>
                                 </div>
-                            </div>
-                            <Button
-                                type="submit"
-                                className={`w-full py-2 transition-all duration-200 ${
-                                    loading ? 'bg-gray-400 cursor-not-allowed' : 'bg-red-600 hover:bg-red-700 active:transform active:scale-95'}`}
-                                disabled={isSubmitDisabled}>
-                                {loading ? (
-                                    <div className="flex items-center justify-center gap-2">
-                                        <Loader2 className="animate-spin"/>
-                                        <span>Entrando...</span>
-                                    </div>) : ('Entrar')}
-                            </Button>
-                        </form>
-                    </CardContent>
-                </Card>
+                                <Button
+                                    type="submit"
+                                    className={`w-full  py-2 transition-all duration-200 ${
+                                        loading ? 'bg-gray-400 cursor-not-allowed' : 'bg-red-900 hover:bg-red-950 active:transform active:scale-95'}`}
+                                    disabled={isSubmitDisabled}>
+                                    {loading ? (
+                                        <div className="flex items-center justify-center gap-2">
+                                            <Loader2 className="animate-spin"/>
+                                            <span>Entrando...</span>
+                                        </div>) : ('Entrar')}
+                                </Button>
+                            </form>
+                        </CardContent>
+                    </Card>
+                </div>
             </div>
-        </div>);
+        </div>
+
+    );
 }
