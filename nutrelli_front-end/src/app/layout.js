958c54f463ad6b9c59e0708ef810a3e41780f14c
Marcos Júnior <marcosajr11@gmail.com>
2025-01-12 11:51:06 -0300

    feat: Added font

--- nutrelli_front-end/src/app/layout.js
+++ nutrelli_front-end/src/app/layout.js
@@ -1,8 +1,16 @@
 import './globals.css';
 
+import { Poppins } from 'next/font/google'
+
+const poppins = Poppins({
+    weight: '400',
+    subsets: ["latin"],
+    display:"swap",
+})
+
 export default function RootLayout({ children }) {
   return (
-      <html lang="en">
+      <html lang="en" className={poppins.className}>
           <head>
               <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
               <title>Nutrelli</title>
