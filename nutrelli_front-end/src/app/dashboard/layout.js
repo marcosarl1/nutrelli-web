944e7c5583ea8db408e9ad05588ac990c5577b8f
Marcos Júnior <marcosajr11@gmail.com>
2025-01-13 14:30:26 -0300

    feat: Responsive layout

--- nutrelli_front-end/src/app/dashboard/layout.js
+++ nutrelli_front-end/src/app/dashboard/layout.js
@@ -9,7 +9,7 @@ export default function DashboardLayout({ children }) {
         <SidebarProvider>
             <AppSidebar />
             <MobileHeader/>
-            <main className="flex-1 p-4 lg:p-2 mt-4 sm:mx-0 md:mx-8 lg:mx-14">
+            <main className="flex-1 p-4 mt-16 lg:mt-4 lg:p-2 sm:mx-0 md:mx-8 lg:mx-14">
                 {children}
             </main>
         </SidebarProvider>
