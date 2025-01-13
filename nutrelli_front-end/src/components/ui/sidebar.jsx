8cb0d288c54d62be391b627840f3fe414c5ef4e1
Marcos Júnior <marcosajr11@gmail.com>
2025-01-13 20:02:12 -0300

    MINOR: added description

--- nutrelli_front-end/src/components/ui/sidebar.jsx
+++ nutrelli_front-end/src/components/ui/sidebar.jsx
@@ -9,7 +9,7 @@ import { cn } from "@/lib/utils"
 import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
 import { Separator } from "@/components/ui/separator"
-import {Sheet, SheetContent, SheetHeader, SheetTitle} from "@/components/ui/sheet"
+import {Sheet, SheetContent, SheetDescription, SheetHeader, SheetTitle} from "@/components/ui/sheet"
 import { Skeleton } from "@/components/ui/skeleton"
 import {
   Tooltip,
@@ -168,7 +168,9 @@ const Sidebar = React.forwardRef((
               "--sidebar-width": SIDEBAR_WIDTH_MOBILE
             }
           }
-          side={side}> <SheetTitle></SheetTitle>
+          side={side}>
+            <SheetTitle></SheetTitle>
+            <SheetDescription></SheetDescription>
           <div className="flex h-full w-full flex-col">{children}</div>
         </SheetContent>
       </Sheet>)
