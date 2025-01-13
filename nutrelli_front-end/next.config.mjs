b6aad7b68727f3a078eeb01fe73b261f21f0ec5a
Marcos Júnior <marcosajr11@gmail.com>
2025-01-13 00:43:19 -0300

    feat: path to images

--- nutrelli_front-end/next.config.mjs
+++ nutrelli_front-end/next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        localPatterns: [
+            {
+                pathname: '/public/**',
+                search: ''
+            }
+        ]
+    }
+};
 
 export default nextConfig;
