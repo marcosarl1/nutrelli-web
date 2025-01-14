cc626846b5fd88a516014f90064e1f099036aa5f
Marcos Júnior <marcosajr11@gmail.com>
2025-01-14 13:48:19 -0300

    cleanup: Not needed annotation

--- src/main/java/com/nutrelliapi/model/OrderedProduct.java
+++ src/main/java/com/nutrelliapi/model/OrderedProduct.java
@@ -1,6 +1,5 @@
 package com.nutrelliapi.model;
 
-import com.fasterxml.jackson.annotation.JsonBackReference;
 import jakarta.persistence.*;
 
 @Entity
@@ -13,7 +12,6 @@ public class OrderedProduct {
 
     @ManyToOne
     @JoinColumn(name = "id_pedido")
-    @JsonBackReference
     private Order order;
 
     @ManyToOne
