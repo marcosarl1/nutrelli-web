fb28326bf7c85592f04a66457800223b40a2f0d9
Marcos Júnior <marcosajr11@gmail.com>
2025-01-14 14:43:26 -0300

    feat: Improved OrderService

--- src/main/java/com/nutrelliapi/service/OrderService.java
+++ src/main/java/com/nutrelliapi/service/OrderService.java
@@ -2,14 +2,18 @@ package com.nutrelliapi.service;
 
 import com.nutrelliapi.model.Order;
 import com.nutrelliapi.model.OrderStatus;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 
 public interface OrderService {
     List<Order> findAllOrders();
     Order findOrderById(Integer id);
+    Page<Order> findAllOrdersPage(Pageable pageable);
+    Page<Order> findOrdersByCustomerContainingIgnoreCase(String customer, Pageable pageable);
+    Page<Order> findOrderByOrderStatus(OrderStatus orderStatus, Pageable pageable);
     Order saveOrder(Order order);
     Order updateOrder(Integer id, Order order);
-    Order updateOrderStatus(Integer id, OrderStatus orderStatus);
     void deleteOrder(Integer id);
 }
--- src/main/java/com/nutrelliapi/service/impl/OrderServiceImpl.java
+++ src/main/java/com/nutrelliapi/service/impl/OrderServiceImpl.java
@@ -1,10 +1,15 @@
 package com.nutrelliapi.service.impl;
 
 import com.nutrelliapi.exception.OrderNotFoundException;
-import com.nutrelliapi.model.Order;
-import com.nutrelliapi.model.OrderStatus;
+import com.nutrelliapi.exception.PaymentTypeNotFoundException;
+import com.nutrelliapi.exception.ProductNotFoundException;
+import com.nutrelliapi.model.*;
 import com.nutrelliapi.repository.OrderRepository;
+import com.nutrelliapi.repository.PaymentTypeRepository;
+import com.nutrelliapi.repository.ProductRepository;
 import com.nutrelliapi.service.OrderService;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -13,14 +18,33 @@ import java.util.List;
 public class OrderServiceImpl implements OrderService {
 
     private final OrderRepository orderRepository;
+    private final PaymentTypeRepository paymentTypeRepository;
+    private final ProductRepository productRepository;
 
-    public OrderServiceImpl(OrderRepository orderRepository) {
+    public OrderServiceImpl(OrderRepository orderRepository, PaymentTypeRepository paymentTypeRepository, ProductRepository productRepository) {
         this.orderRepository = orderRepository;
+        this.paymentTypeRepository = paymentTypeRepository;
+        this.productRepository = productRepository;
     }
 
     @Override
     public List<Order> findAllOrders() {
         return orderRepository.findAll();
+    }
+
+    @Override
+    public Page<Order> findAllOrdersPage(Pageable pageable) {
+        return orderRepository.findAll(pageable);
+    }
+
+    @Override
+    public Page<Order> findOrdersByCustomerContainingIgnoreCase(String customer, Pageable pageable) {
+        return orderRepository.findOrdersByCustomerContainingIgnoreCase(customer, pageable);
+    }
+
+    @Override
+    public Page<Order> findOrderByOrderStatus(OrderStatus orderStatus, Pageable pageable) {
+        return orderRepository.findOrderByOrderStatus(orderStatus, pageable);
     }
 
     @Override
@@ -30,26 +54,19 @@ public class OrderServiceImpl implements OrderService {
 
     @Override
     public Order saveOrder(Order order) {
+        validatePaymentType(order);
+        setDefaultOrderStatus(order);
+        validateOrderedProducts(order);
+        calculateTotalValue(order);
         return orderRepository.save(order);
     }
 
     @Override
     public Order updateOrder(Integer id, Order order) {
-        return orderRepository.findById(id)
-                .map(existingOrder -> {
-                    order.setId(id);
-                    return orderRepository.save(order);
-                }).orElseThrow(() -> new OrderNotFoundException("Pedido não encontrado"));
-    }
-
-    @Override
-    public Order updateOrderStatus(Integer id, OrderStatus orderStatus) {
-        Order order = orderRepository.findById(id).orElseThrow(() -> new OrderNotFoundException("Pedido não encontrado"));
-        if (order.getOrderStatus() == OrderStatus.FINALIZADO || order.getOrderStatus() == OrderStatus.CANCELADO) {
-            throw new OrderNotFoundException("Não é possível alterar o status de um pedido finalizado ou cancelado");
-        }
-        order.setOrderStatus(orderStatus);
-        return orderRepository.save(order);
+        Order existingOrder = findOrderById(id);
+        updateExistingOrder(existingOrder, order);
+        calculateTotalValue(existingOrder);
+        return orderRepository.save(existingOrder);
     }
 
     @Override
@@ -58,5 +75,47 @@ public class OrderServiceImpl implements OrderService {
             throw new OrderNotFoundException("Pedido não encontrado");
         }
         orderRepository.deleteById(id);
+    }
+
+    private void validatePaymentType(Order order) {
+        if (order.getPaymentType() != null) {
+            PaymentType paymentType = paymentTypeRepository
+                    .findById(order.getPaymentType().getId())
+                    .orElseThrow(() -> new PaymentTypeNotFoundException("Tipo de pagamento não encontrado"));
+            order.setPaymentType(paymentType);
+        }
+    }
+
+    private void setDefaultOrderStatus(Order order) {
+        order.setOrderStatus(OrderStatus.PENDENTE);
+    }
+
+    private void validateOrderedProducts(Order order) {
+        if (order.getOrderedProducts() != null) {
+            for (OrderedProduct orderedProduct : order.getOrderedProducts()) {
+                Product product = productRepository
+                        .findById(orderedProduct.getProduct().getId())
+                        .orElseThrow(() -> new ProductNotFoundException("Produto não encontrado"));
+                orderedProduct.setProduct(product);
+                orderedProduct.setOrder(order);
+            }
+        }
+    }
+
+    private void updateExistingOrder(Order existingOrder, Order order) {
+        existingOrder.setCustomer(order.getCustomer());
+        existingOrder.setOrderStatus(order.getOrderStatus());
+        validatePaymentType(order);
+        existingOrder.getOrderedProducts().clear();
+        validateOrderedProducts(order);
+        existingOrder.getOrderedProducts().addAll(order.getOrderedProducts());
+    }
+
+    private void calculateTotalValue(Order order) {
+        double total = order.getOrderedProducts()
+                .stream()
+                .mapToDouble(op -> op.getQuantity() * op.getProduct().getPrice())
+                .sum();
+        order.setTotalValue(total);
     }
 }
