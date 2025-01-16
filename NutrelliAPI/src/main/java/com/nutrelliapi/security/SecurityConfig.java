6b8fb9726d42450225bc599ba065550b0d1cdda9
Marcos Júnior <marcosajr11@gmail.com>
2025-01-16 18:52:16 -0300

    chore: Prod settings

--- NutrelliAPI/src/main/java/com/nutrelliapi/security/SecurityConfig.java
+++ NutrelliAPI/src/main/java/com/nutrelliapi/security/SecurityConfig.java
@@ -46,7 +46,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:3000", "http://192.168.0.7:3000"));
+        configuration.setAllowedOrigins(List.of("http://localhost:3000", "http://192.168.0.7:3000", "https://nutrelli-web.vercel.app/"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
