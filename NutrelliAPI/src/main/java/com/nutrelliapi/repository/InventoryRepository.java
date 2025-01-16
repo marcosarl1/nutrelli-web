c39ee67fe036e421022d0156f648bbb20543d40d
Marcos Júnior <marcosajr11@gmail.com>
2025-01-16 12:32:10 -0300

    fix: Using custom query

--- NutrelliAPI/src/main/java/com/nutrelliapi/repository/InventoryRepository.java
+++ NutrelliAPI/src/main/java/com/nutrelliapi/repository/InventoryRepository.java
@@ -4,10 +4,13 @@ import com.nutrelliapi.model.Inventory;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface InventoryRepository extends JpaRepository<Inventory, Integer> {
     Page<Inventory> findByNameContainingIgnoreCase(String name, Pageable pageable);
-    Page<Inventory> findByQuantityIsLessThan(Double minimumQuantity, Pageable pageable);
+
+    @Query("SELECT i FROM Inventory i WHERE i.quantity < i.minimumQuantity")
+    Page<Inventory> findLowStockItems(Pageable pageable);
 }
