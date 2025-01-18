f98fb45dcb5addf8278ca62108d0fbe09acf0318
Marcos Júnior <marcosajr11@gmail.com>
2025-01-18 00:55:20 -0300

    feat: PATCH allowed methods

--- NutrelliAPI/src/main/java/com/nutrelliapi/security/SecurityConfig.java
+++ NutrelliAPI/src/main/java/com/nutrelliapi/security/SecurityConfig.java
@@ -48,7 +48,7 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedOrigins(List.of("http://localhost:3000", "http://192.168.0.7:3000", "https://nutrelli-web.vercel.app/"));
-        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
