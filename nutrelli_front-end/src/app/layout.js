3e11279139d63cac31b99c7e17fcf2cd830eb765
Marcos Júnior <marcosajr11@gmail.com>
2025-01-17 21:14:09 -0300

    fix: Font works properly

--- nutrelli_front-end/src/app/layout.js
+++ nutrelli_front-end/src/app/layout.js
@@ -10,12 +10,12 @@ const poppins = Poppins({
 
 export default function RootLayout({ children }) {
   return (
-      <html lang="en" className={poppins.className}>
+      <html lang="en">
           <head>
               <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
               <title>Nutrelli</title>
           </head>
-          <body>{children}
+          <body  className={poppins.className}>{children}
           </body>
       </html>
   );
