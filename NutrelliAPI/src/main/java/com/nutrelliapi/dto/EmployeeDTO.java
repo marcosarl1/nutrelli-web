22a604a99faae455b77a76389f5593f1c9561704
Marcos Júnior <marcosajr11@gmail.com>
2025-01-19 00:23:20 -0300

    feat: EmployeeDTO Validation

--- NutrelliAPI/src/main/java/com/nutrelliapi/dto/EmployeeDTO.java
+++ NutrelliAPI/src/main/java/com/nutrelliapi/dto/EmployeeDTO.java
@@ -1,8 +1,22 @@
 package com.nutrelliapi.dto;
 
+import jakarta.persistence.Column;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
 public class EmployeeDTO {
+
+    @NotBlank(message = "O nome é obrigatório")
+    @Size(max = 100, message = "O nome deve ter no máximo 100 caracteres")
     private String name;
+
+    @NotBlank(message = "O email é obrigatório")
+    @Email(message = "O email deve ser válido")
     private String email;
+
+    @Column(name = "senha")
+    @NotBlank(message = "A senha é obrigatória")
     private String password;
 
     public String getName() {
